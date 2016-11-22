89e608a7953ac286affb375a490b7d0a468b627d
William-Weng <linuxice0609@gmail.com>
2016-11-22 13:56:27 +0800

    no message

--- Reading/Class.playground/Contents.swift
+++ Reading/Class.playground/Contents.swift
@@ -96,3 +96,7 @@ print(triangleAndSquare.square.sideLength)
 print(triangleAndSquare.triangle.sideLength)
 triangleAndSquare.square = Square(sideLength: 50, name: "larger square")
 print(triangleAndSquare.triangle.sideLength)
+
+
+let optionalSquare: Square? = Square(sideLength: 2.5, name: "optional square")
+let sideLength = optionalSquare?.sideLength
